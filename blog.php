8c2b1e3f0149fc60b8f13f5a71f40987171a1416
Miguel Arias <ariasmiguel0316@gmail.com>
2021-05-25 22:13:16 -0500

    archivos

--- blog.php
+++ blog.php
@@ -41,13 +41,13 @@
   <header id="header" class="fixed-top header-inner-pages">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">Tempo</a></h1>
+      <h1 class="logo mr-auto"><a href="index.php">Tempo</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li><a href="index.html">Inicio</a></li>
+          <li><a href="index.php">Inicio</a></li>
           <li><a href="#about">Nosotros</a></li>
           <li><a href="#services">Servicios</a></li>
           <li><a href="#portfolio">Galeria</a></li>
