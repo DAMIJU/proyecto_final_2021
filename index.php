47a4a3cdf48fdc102746f68ec687c52172e79e6d
DarwinMeneses <84646812+DarwinMeneses@users.noreply.github.com>
2021-06-23 09:38:30 -0500

    Anexo Darwin Index

--- index.php
+++ index.php
@@ -196,20 +196,6 @@
 
       <div class="container">
 
-        <div class="section-title">
-          <h2>Team</h2>
-          <h3>Titulo de ejemplo <span>123</span></h3>
-          <p>Diego León Castaño Castaño adiestrador canino profesional con más de 15 años de experiencia.<p>
-
-          <p>Diplomado en la escuela de adiestramiento Canino Handler Colombia en el 2008.<p>
-
-          <p>Participación en el 1er seminario internacional de adiestramiento Canino de perros de trabajo en la universidad UANL en noviembre del 2009 en coordinación con la fuerza aérea Colombiana.<p>
-
-          <p>Participación en seminarios de IGP, OCI, con los campeones mundiales. Valentina Balli, Arnold Kivago, Miriam Sodlengard.<p>
-
-          <p>Adiestrador Canino calificado K-nes de la escuela Colombia de adiestramiento, con énfasis en educación y modificación de conducta canina</p>
-        </div>
-
         <div class="m-0 vh-90 row justify-content-center align-items-center">
           <div class="col-lg-4 col-md-8 d-flex align-items-stretch">
             <div class="member">
@@ -230,17 +216,22 @@
         </div> 
         <div class="fondo_transparente">
         <div class="modal">
-            <div class="modal_cerrar">
-                <span>x</span>
-            </div>
-            <div class="modal_titulo">VENTANA MODAL</div>
-            <div class="modal_mensaje">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit, nam? Minus nihil temporibus, minima reprehenderit, rem explicabo earum nemo debitis, maxime deserunt quidem. Quia odit quae voluptate nobis sit beatae!</p>
-            </div>
-            <div class="modal_botones">
-                <a href="" class="boton">COMPARTIR</a>
-                <a href="" class="boton">ACEPTAR</a>
-            </div>
+            
+           <a class="modal_cerrar"> <i class="fas fa-times"></i></a>
+            
+            <!-- <div class="modal_titulo">VENTANA MODAL</div> -->
+            <div class="section-title">
+            <br>
+          <p>Diego León Castaño Castaño adiestrador canino profesional con más de 15 años de experiencia.<p>
+
+          <p>Diplomado en la escuela de adiestramiento Canino Handler Colombia en el 2008.<p>
+
+          <p>Participación en el 1er seminario internacional de adiestramiento Canino de perros de trabajo en la universidad UANL en noviembre del 2009 en coordinación con la fuerza aérea Colombiana.<p>
+
+          <p>Participación en seminarios de IGP, OCI, con los campeones mundiales. Valentina Balli, Arnold Kivago, Miriam Sodlengard.<p>
+
+          <p>Adiestrador Canino calificado K-nes de la escuela Colombia de adiestramiento, con énfasis en educación y modificación de conducta canina</p>
+        </div>
         </div>
     </div>   
       </section> 
