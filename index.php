d1fba48d62c8b1d6d01ec26e9023565a13d844f3
Miguel Arias <ariasmiguel0316@gmail.com>
2021-06-24 21:29:27 -0500

    archivos

--- index.php
+++ index.php
@@ -67,7 +67,7 @@
       <!-- <h3>Bienvenidos</h3> -->
       <h1 style="font-family: 'Audiowide', cursive;">Servicios Caninos Casme</h1>
       <h2>Escuela profesional de adiestramiento canino</h2>
-      <a id="btn-abrir-popup3" class="btn-get-started btn btn-lg scrollto span2">Contactenos</a>   
+      <a id="btn-abrir-popup" class="btn-get-started btn btn-lg scrollto span2">Contactenos</a>   
     </div>
     <!-- <div class="container"> -->
        <div class="contenedor">
@@ -123,78 +123,11 @@
           <!-- <a class="cta-btn" href="#">Ir</a> -->
         </div>
       </div>
-    </section>
-    <section>
-    
-      
-    </section>
+    </section> 
     <!-- End Cta Section -->
-    <!-- ======= F.A.Q Section ======= -->
-    <br></br>    
-    <!-- <section id="faq" class="faq">
-      <div class="container">
-        <div class="section-title">
-           <h2>F.A.Q</h2> 
-          <h3>Preguntas<span> Frecuentes</span></h3>
-        </div>
-        <ul class="faq-list">
-          <li>
-            <a data-toggle="collapse" class="" href="#faq1">¿Es esta una pregunta de prueba? <i class="icofont-simple-up"></i></a>
-            <div id="faq1" class="collapse show" data-parent=".faq-list">
-              <p>
-                Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-              </p>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#faq2" class="collapsed">¿Es esta una pregunta de prueba?  <i class="icofont-simple-up"></i></a>
-            <div id="faq2" class="collapse" data-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#faq3" class="collapsed">¿Es esta una pregunta de prueba?  <i class="icofont-simple-up"></i></a>
-            <div id="faq3" class="collapse" data-parent=".faq-list">
-              <p>
-                Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
-              </p>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#faq4" class="collapsed">¿Es esta una pregunta de prueba?  <i class="icofont-simple-up"></i></a>
-            <div id="faq4" class="collapse" data-parent=".faq-list">
-              <p>
-                Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-              </p>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#faq5" class="collapsed">¿Es esta una pregunta de prueba?  <i class="icofont-simple-up"></i></a>
-            <div id="faq5" class="collapse" data-parent=".faq-list">
-              <p>
-                Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-              </p>
-            </div>
-          </li>
-          <li>
-            <a data-toggle="collapse" href="#faq6" class="collapsed">¿Es esta una pregunta de prueba?  <i class="icofont-simple-up"></i></a>
-            <div id="faq6" class="collapse" data-parent=".faq-list">
-              <p>
-                Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
-              </p>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </section> -->
-    <!-- End F.A.Q Section -->
-    <br><br>
     <!-- ======= Team Section ======= -->
     
     <section id="team" class="team">
-
       <div class="container">
 
         <div class="m-0 vh-90 row justify-content-center align-items-center">
