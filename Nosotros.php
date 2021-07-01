468323b0fdefeb3968ea5dc7878dc8f999232b29
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-07-01 11:10:46 -0500

    .

--- Nosotros.php
+++ Nosotros.php
@@ -89,30 +89,6 @@
   </div>
 </section>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" id="ModalEnsayo" data-toggle="modal" data-target="#staticBackdrop">
-  Launch static backdrop modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Understood</button>
-      </div>
-    </div>
-  </div>
-</div>
 <!-- End About Section -->
     <!-- ======= Footer ======= -->
   <footer id="footer">
