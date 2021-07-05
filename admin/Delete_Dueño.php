2f16fe33c88eb010e638bd77b74e68ef69bc341b
DarwinMeneses <84646812+DarwinMeneses@users.noreply.github.com>
2021-07-05 15:22:52 -0500

    Revert "Anexo Darwin"
    This reverts commit a3a54fce5a7443a27b1dd163ff5ecb16445b56b3.

--- admin/Delete_Dueño.php
+++ admin/Delete_Dueño.php
@@ -2,19 +2,15 @@
 
 include("DB/conexion.php");
 
-if(isset($_GET['Celular'])) {
-  $Celular = $_GET['Celular'];
-  $query2="SELECT * FROM tabla_dueño INNER JOIN tabla_mascotas ON tabla_dueño.Celular = tabla_mascotas.Cel_Dueño WHERE Celular='$Celular'";
-      $resultado= $con->query($query2);
-      $row=$resultado->fetch_assoc();
-  $id = $row['Celular'];    
-  $query = "DELETE FROM tabla_dueño WHERE Celular = $Celular";
+if(isset($_GET['Doc'])) {
+  $Doc = $_GET['Doc'];
+  $query = "DELETE FROM dueño WHERE Doc = $Doc";
   $result = mysqli_query($con, $query);
   if(!$result) {
     die("Query Failed.");
   }
 
-  $_SESSION['message'] = 'El cliente <?php echo $id ?> fue eliminado correctamente';
+  $_SESSION['message'] = 'Task Removed Successfully';
   $_SESSION['message_type'] = 'danger';
   header('Location: Dueño.php');
 }
