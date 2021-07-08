ed356ce128f16f8ab0d647bfdd6cd10073e836a5
Miguel Arias <ariasmiguel0316@gmail.com>
2021-07-08 15:05:55 -0500

    Update Añadir_Dueño.php
    Dueño ya registrado alerta !

--- admin/Añadir_Dueño.php
+++ admin/Añadir_Dueño.php
@@ -11,15 +11,19 @@ $Telefono_Fijo = $_POST['Telefono_Fijo'];
 $Dirección = $_POST['Dirección'];
 $Ciudad = $_POST['Ciudad'];
 $Correo = $_POST['Correo'];
-$query = "INSERT INTO tabla_dueño (Num_Registro_Dueño,Celular,Nombre_Dueño,Telefono_Fijo,Dirección,Ciudad,Correo,Fecha_Registro_Dueño) VALUES (NULL,'$Celular', '$Nombre_Dueño', '$Telefono_Fijo', '$Dirección', '$Ciudad', '$Correo', CURDATE())";
-$ResultadoAgregarDueño = $con->query($query);
-
-if($ResultadoAgregarDueño){
-echo "<script>alert('El cliente $Nombre_Dueño se ha agregado satisfactoriamente');window.location='Dueño.php'</script>";
+$query="SELECT Celular FROM tabla_dueño WHERE Celular = $Celular";
+$resultado= $con->query($query);
+$row=$resultado->fetch_assoc();
+if($row>0){
+  echo "<script>alert('El dueño con el numero $Celular ya se encuentra registrado');window.location='Dueño.php'</script>";
 }else{
-  echo "<script>alert('los datos no se han podido guardar correctamente');</script>";
+  $query = "INSERT INTO tabla_dueño (Num_Registro_Dueño,Celular,Nombre_Dueño,Telefono_Fijo,Dirección,Ciudad,Correo,Fecha_Registro_Dueño) VALUES (NULL,'$Celular', '$Nombre_Dueño', '$Telefono_Fijo', '$Dirección', '$Ciudad', '$Correo', CURDATE())";
+$ResultadoAgregarDueño = $con->query($query);
+}
+if($ResultadoAgregarDueño){
+  echo "<script>alert('El cliente $Nombre_Dueño se ha agregado satisfactoriamente');window.location='Dueño.php'</script>";
+}else{
+  echo "<script>alert('los datos no se han podido guardar correctamente');window.location='Dueño.php'</script>";
 }
 }
-    
-
 ?>
