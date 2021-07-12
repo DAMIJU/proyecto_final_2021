37de1e90391b8db617d60ca420de6b25199a53c1
Miguel Arias <ariasmiguel0316@gmail.com>
2021-07-12 01:33:48 -0500

    Logo edit

--- Login.php
+++ Login.php
@@ -57,7 +57,7 @@
   <!-- ======= Header ======= -->
   <header id="header" class="">
     <div class="container d-flex align-items-center">
-      <h1 class="logo mr-auto"><a href="index.php"><img src="assets/img/Logo sin fondo.png" alt="Caninos Casme"></a></h1>
+      <h1 class="logo mr-auto"><a href="index.php"><img src="assets/img/Logo.png" alt="Caninos Casme"></a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       <nav class="nav-menu d-none d-lg-block">
