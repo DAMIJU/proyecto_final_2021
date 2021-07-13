c6fe556aff18f24adca9cb23193c64a513083fe6
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-07-13 17:00:41 -0500

    .

--- Nosotros.php
+++ Nosotros.php
@@ -39,27 +39,23 @@
   
  <!-- <button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button> -->
 <!-- ======= Header ======= -->
-<header id="header" class="fixed-top ">
+<header id="header" class="">
     <div class="container d-flex align-items-center">
+      <h1 class="logo mr-auto"><a href="#"><img src="assets/img/Logo.png" alt="Logo Caninos Casmes"></a></h1>
 
-      <h1 class="logo mr-auto"><a href="index.php"><img src="assets/img/Logo.png" alt="Caninos Casme"></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li><a href="index.php">Inicio</a></li>
-          <li><a href="Nosotros.php">Nosotros</a></li>
+          <li class="active"><a href="Nosotros.php">Nosotros</a></li>
           <li><a href="Servicios.php">Servicios</a></li>
-          <li><a href="Galeria.php">Galería</a></li>                  
+          <li><a href="Galeria.php">Galería</a></li>
           <li><a href="Login.php">Admin</a></li>
         </ul>
-      </nav><!-- .nav-menu -->
+      </nav>
     </div>
   </header>
   <!-- End Header -->
-  <!-- <br><br><br><br><br><br><br> -->
-<!-- ======= About Section ======= -->
-<br><br><br><br><br><br>
+  <br>
 <section class="page-title" style="background-image:url('assets/img/Banner_Conocenos.png');background-repeat: no-repeat;background-size: cover;">
         <div class="auto-container">
            <br> <h1 style="color:white;text-align:center;font-weight:bold">CONÓCENOS</h1>
