a229f42ae41a2d3eeecf47fce82c095e76e9fb3b
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-07-19 16:03:16 -0500

    Comentarios y código organizado en el archivo login

--- Login.php
+++ Login.php
@@ -5,19 +5,16 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>Panel de administrador</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  <meta content="Miguel Angel Arias, Darwin Meneses, Juan Esteban Alvarez" name="autor">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+    <meta content="Miguel Angel Arias, Darwin Meneses, Juan Esteban Alvarez" name="autor">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <!--JQUERY-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
-    <!-- FRAMEWORK BOOTSTRAP para el estilo de la pagina-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     
-    <!-- Los iconos tipo Solid de Fontawesome-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/solid.css">
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
     <script src="assets/js/CaninosCasmeLogin.js"></script>
@@ -29,8 +26,6 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-    
-    <!-- Vendor CSS Files -->
     <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
     <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
@@ -41,25 +36,13 @@
     <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600|Open+Sans" rel="stylesheet"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-    <!-- Template Main CSS File -->
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/LoginStyles.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Tempo - v2.2.1
-  * Template URL: https://bootstrapmade.com/tempo-free-onepage-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 <body>
-<!--   <button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button> -->
-  <!-- ======= Header ======= -->
   <header id="header" class="">
     <div class="container d-flex align-items-center">
       <h1 class="logo mr-auto"><a href="index.php"><img src="assets/img/Logo.png" alt="Caninos Casme"></a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       <nav class="nav-menu d-none d-lg-block">
         <ul>
             <li><a href="index.php">Inicio</a></li>
@@ -68,36 +51,33 @@
             <li ><a href="Galeria.php">Galería</a></li>              
             <li class="active"><a href="Login.php">Admin</a></li>
         </ul>
-      </nav><!-- .nav-menu -->
+      </nav>
     </div>
-  </header><!-- End Header -->
+  </header>
   <div class="global-container">
-	<div class="card login-form" style="margin-top:-100px">
-	<div class="card-body">
-		<h4 style="font-weight:bold" class="card-title text-center">Panel Administrador</h4>
-		<div class="card-text">
-			<!--
-			<div class="alert alert-danger alert-dismissible fade show" role="alert">Incorrect username or password.</div> -->
-			<form action="admin/validar.php" method="POST" id="Form_InicioSesion">
-				<!-- to error: add class "has-danger" -->
-				<div class="form-group">
-					<label for="exampleInputEmail1">Nombre de usuario<span style="color:red">*</span></label>
-					<input type="text" name="username" class="form-control form-control-sm" placeholder="Usuario" maxlength="10" id="login-name">
-				</div>
-				<div class="form-group">
-					<label for="exampleInputPassword1">Contraseña<span style="color:red">*</span></label>
-					<a href="#" onclick="alert('Acción aún no disponible')" style="float:right;font-size:12px;">¿Olvidó su contraseña?</a>
-					<input type="password" name="password" class="form-control form-control-sm" placeholder="Contraseña" maxlength="" id="login-pass">
-				</div>
-				<button type="submit" class="btn btn_AccederNewLogin btn-block">Acceder</button>
-			</form>
-		</div>
-	</div>
-</div>
-<!-- Vendor JS Files -->
+	  <div class="card login-form" style="margin-top:-100px">
+	    <div class="card-body">
+		    <h4 style="font-weight:bold" class="card-title text-center">Panel Administrador</h4>
+		    <div class="card-text">
+			    <form action="admin/validar.php" method="POST" id="Form_InicioSesion">
+				    <div class="form-group">
+					    <label for="exampleInputEmail1">Nombre de usuario<span style="color:red">*</span></label>
+					    <input type="text" name="username" class="form-control form-control-sm" placeholder="Usuario" maxlength="10" id="login-name">
+				    </div>
+				    <div class="form-group">
+					    <label for="exampleInputPassword1">Contraseña<span style="color:red">*</span></label>
+					    <a href="#" onclick="alert('Acción aún no disponible')" style="float:right;font-size:12px;">¿Olvidó su contraseña?</a>
+					    <input type="password" name="password" class="form-control form-control-sm" placeholder="Contraseña" maxlength="" id="login-pass">
+				    </div>
+				  <button type="submit" class="btn btn_AccederNewLogin btn-block">Acceder</button>
+			    </form>
+		  </div>
+	  </div>
+  </div>
+
 <script src="assets/vendor/jquery/jquery.min.js"></script>
 <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- Template Main JS File -->
+
 <script src="assets/js/main.js"></script>
 </body>
 </html>
