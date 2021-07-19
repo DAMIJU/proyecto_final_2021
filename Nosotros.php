2edcf73dd214406413db3e97a6872bd71c09543f
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-07-19 15:28:18 -0500

    Comentarios y código organizado del archivo nosotros

--- Nosotros.php
+++ Nosotros.php
@@ -25,20 +25,9 @@
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
   
-  <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Tempo - v2.2.1
-  * Template URL: https://bootstrapmade.com/tempo-free-onepage-bootstrap-theme/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 <body>
-  
- <!-- <button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button> -->
-<!-- ======= Header ======= -->
 <header id="header" class="">
     <div class="container d-flex align-items-center">
       <h1 class="logo mr-auto"><a href="index.php"><img src="assets/img/Logo.png" alt="Logo Caninos Casmes"></a></h1>
@@ -54,14 +43,12 @@
       </nav>
     </div>
   </header>
-  <!-- End Header -->
   <br>
-<!-- End Header -->
 <br>
 <section class="page-title" style="background-image:url('assets/img/Banner_Conocenos.png');background-repeat: no-repeat;background-size: cover;">
-        <div class="auto-container">
-           <br> <h1 style="color:white;text-align:center;font-weight:bold">CONÓCENOS</h1>
-        </div>
+  <div class="auto-container">
+     <br> <h1 style="color:white;text-align:center;font-weight:bold">CONÓCENOS</h1>
+  </div>
 </section>
 <section>
   <div class="container">
@@ -99,33 +86,6 @@
   </div>
 </div>
   </div><br><br>
-<!-- <section id="" class="about">
-  <div class="container">  
-    <div class="row content">
-      <div class="et_pb_row et_pb_row_0">
-        <p> Casme es un centro de servicios caninos, ubicado en Tantoyuca Veracruz México, es un lugar que cuenta con más de 16 años de experiencia, donde se prestan servicios como estética canina, educación y modificación de conducta, asesorías, venta de mascotas y productos. Se ha mantenido un crecimiento en lo que respecta al número de clientes y nivel de ingreso, lo cual es producto de una constante búsqueda de satisfacción al cliente, entregándoles una mayor variedad de servicios y prestaciones.</p>       
-      </div>
-      <div class="container">
-        <div class="et_pb_row et_pb_row_0">
-          <div class="section-mision">
-            <h3><span> Nuestra Misión</span></h3>
-            <p>Consentir y embellecer a tu mascota mediante un servicio de calidad y profesionalismo, suminiestrando accesorios de calidad necesarios para una sana y alegre convivencia</p>       
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="et_pb_row et_pb_row_0">
-          <div class="section-vision">
-            <h3><span> Nuestra Visión</span></h3>
-            <p>Ser una empresa reconocida por toda la región y recomendada por su calidad y buen servicio donde se encuentre la solución a necesidades como: elección de mascota, embellecimiento, orientación psicosocial e integración familiar.</p>  
-        </div>
-      </div>
-    </div>
-  </div>
-</section> -->
-  <!-- End Services Section -->
- <!-- ======= Footer ======= -->
- 
  <footer id="footer">
     <div class="container d-md-flex py-4">
       <div class="mr-md-auto text-center text-md-left">
@@ -133,11 +93,6 @@
           &copy; Copyright <strong><span>Casme</span></strong>. Todos los derechos reservados.
         </div>
         <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/tempo-free-onepage-bootstrap-theme/ -->
-         <!--  Diseñado por <a href="#">Caninos Casme</a> -->
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
@@ -145,10 +100,9 @@
         <a href="https://www.facebook.com/Servicios-caninos-Casme-905518146207815" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>     
         <a href="https://www.instagram.com/servicioscaninoscasme/" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
         <a href="https://wa.me/527891017905" target="_blank" class="google-plus"><i class="bx bxl-whatsapp"></i></a>
-        <!-- <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
 
   <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
 
