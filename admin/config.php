3becf74d588661eb56529203db2cb4c5c4de7de2
Juanes Álvarez <juanestebanalvarezholguin@gmail.com>
2021-09-21 22:32:39 -0500

    .

--- admin/config.php
+++ admin/config.php
@@ -1398,8 +1398,351 @@ if (!isset($_SESSION['loggedin'])) {
   <div class="content-header admin-panel">      
     <div class="text-center">
       <h1>CONFIGURACIÓN</h1>
-    </div>   
+    </div>
+    <a href="Config.php?modulo=ViewAdmin"><button class="btn btn-info">Admin registrados</button></a>
   </div>
+ 
+  <div class="wrapper2">
+      <div class="card">
+        <div class="card-header card-primary">
+          <h2 class="title">Añadir admin</h2>
+        </div>
+        <div class="card-body">
+        <form action="AgregarUser.php" method="POST" autocomplete="OFF">
+          <!-- 2 column grid layout with text inputs for the first and last names -->
+          <div class="row mb-4">
+            <div class="col">
+              <div class="form-outline">
+                <label class="form-label FormLogin" for="NombreCompleto">Nombres<span style="color:red">*</span></label>
+                <input type="text" class="form-control" name="FullName">
+              </div>
+            </div>
+            <div class="col">
+              <div class="form-outline">
+                <label class="form-label FormLogin" for="Apellido">Apellidos<span style="color:red">*</span></label>
+                <input type="text" id="Apellido" required="required" name="Apellidos_Usuario" class="form-control" />
+              </div>
+            </div>
+          </div>
+
+          <div class="form-outline mb-4">
+            <label class="form-label FormLogin" for="usuario">Número de documento<span style="color:red">*</span></label>
+            <input type="number" name="id_user" required="required" id="id_user" class="form-control" />
+          </div> 
+
+          <!-- Text input -->
+          <div class="form-outline mb-4">
+            <label class="form-label FormLogin" for="usuario">Nombre de Usuario<span style="color:red">*</span></label>
+            <input type="text" name="usuario" required="required" id="usuario" class="form-control" />
+          </div>
+
+          <!-- Text input -->
+          <div class="form-outline mb-4">
+            <label class="form-label FormLogin" for="password">Contraseña<span style="color:red">*</span></label>
+            <input type="password" name="password" id="password" class="form-control" />
+          </div>
+
+<!--           <div class="form-outline mb-4">
+            <label class="form-label FormLogin" for="password">Confirmar Contraseña<span style="color:red">*</span></label>
+            <input type="password" name="password2" id="password2" class="form-control" />
+          </div> -->
+          <input type="submit" name="GuardarNuevoUsuarioAdmin" class="btn btn-success btn-block mb-4" value="Guardar">
+        </form>
+        </div>
+      </div>
+      </div>  
+  </div> 
+	</div>
+<!-- DATATABLE DUEÑO -->
+  
+   <footer class="main-footer">
+      <div class="float-right d-none d-sm-block">
+        <b>Servicios Caninos Casme.</b> 
+      </div>
+        <strong>Panel de administrador</a></strong> 
+    </footer>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- jQuery UI -->
+<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- fullCalendar 2.2.5 -->
+<script src="../plugins/moment/moment.min.js"></script>
+<script src="../plugins/fullcalendar/main.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+<!-- Page specific script -->
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
+<!-- SCRIPTS DataTables -->
+<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+<script src="select2/select2.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function(){
+		$('#controlBuscador').select2({ dropdownParent: "#staticBackdrop" });
+	});
+</script>
+</body>
+<?php 
+    }
+?>
+<?php 
+    if ($_GET['modulo'] == 'ViewAdmin') {
+  ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Panel de admin</title>
+
+  <!-- Favicons -->
+  <link href="../assets/img/Logo.ico" rel="icon">
+  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- fullCalendar -->
+  <link rel="stylesheet" href="../plugins/fullcalendar/main.css">
+
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+  <!-- Template Main CSS File -->
+  <link href="../assets/css/style.css" rel="stylesheet">
+  <!-- Bootstrap para DataTables -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap4.min.css">
+  <!-- Vendor CSS Files -->
+  <link href="../assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="../assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="../assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="../assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <style>
+    li{
+      list-style: none;
+    }
+
+    label .FormLogin{
+      font-weight: 1;
+    }
+  </style>
+  <link rel="stylesheet" type="text/css" href="select2/select2.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+      <img class="animation__shake" src="../assets/img/Logo.png" alt="Casme Logo" height="90px" width="100px">
+    </div>
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+        </li>
+      </ul>
+      <ul class="navbar-nav ml-auto">
+        <!-- Navbar Search -->      
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="modal" data-target="#exitModal">
+            <i class="icofont-exit" aria-hidden="true"></i> Salir
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <aside class="main-sidebar sidebar-dark-primary elevation-4" style="color:white">  
+    <div class="sidebar">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../assets/img/Logo.ico" rel="icon" class="brand-image img-circle elevation-4" style="opacity: .8">
+        </div>
+        <div class="info">
+          <span class="brand-text font-weight-light" style="font-size:2.5vh"><?php echo $row['Apellidos_Usuario']?><br><?php echo $row['Nombre_Usuario']?></span>
+        </div>
+      </div>
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">       
+        <li class="nav-item menu-open">
+          <a href="Admin.php" class="nav-link">
+            <i class="nav-icon fa fa-dashboard"></i>
+            <p>
+              Inicio
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="Dueño.php" class="nav-link">
+            <i class="nav-icon fas fa-users"></i>
+            <p>
+              Dueño            
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="Mascota.php" class="nav-link">
+            <i class="nav-icon fas fa-dog"></i>
+            <p>
+              Mascota             
+            </p>
+          </a>     
+        </li>   
+        <li class="nav-item">
+          <a href="Citas.php" class="nav-link">
+            <i class="nav-icon fa fa-calendar-alt"></i>
+            <p>
+              Citas
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://docs.google.com/spreadsheets/d/1yrcoWa9NW8_W0c331Dn6I7Eo7khiJc_bRkr7Gh91E04/edit?usp=sharing" target="_blank" class="nav-link">
+            <i class="nav-icon fas fa-clipboard"></i>
+            <p>
+              Form educación
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://docs.google.com/spreadsheets/d/14CeUn7SXvCSbLqx6X43VQaimSk4uE7s4iQjCJiLNsxc/edit?usp=sharing" target="_blank" class="nav-link">
+            <i class="nav-icon fas fa-clipboard"></i>
+            <p>
+              Form adiestramiento
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="https://docs.google.com/spreadsheets/d/1UpHaj2hp8583jv18H2mekoe2OOlT6WxSP-NTTzLo1D8/edit?usp=sharing" target="_blank" class="nav-link">
+            <i class="nav-icon fas fa-clipboard"></i>
+            <p>
+              Form modificación conductual
+            </p>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a href="Configuracion.php" class="nav-link active">
+            <i class="nav-icon fas fa-cogs"></i>
+            <p>
+              Configuración
+            </p>
+            <i class="fas fa-angle-left right"></i>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="Config.php?modulo=Galeria" class="nav-link">
+                <i class="fas fa-photo-video nav-icon"></i>
+                <p>Galería de imágenes</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="Config.php?modulo=AddUser" class="nav-link">
+                <i class="fas fa-user nav-icon"></i>
+                <p>Añadir admin</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="Config.php?modulo=AddUser" class="nav-link">
+                <i class="fas fa-unlock nav-icon"></i>
+                <p>Contraseñas</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </nav>
+    </div>
+  </aside>
+</div>
+<!-- MODAL PARA CERRAR SESIÓN -->
+<div class="modal fade" id="exitModal" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h3 class="modal-title" id="staticBackdropLabel" style="font-weight:bold">¿Desea salir?</h3>
+      </div>
+      <div class="modal-body">Presione "Cerrar Sesión" si desea salir.</div>
+        <div class="modal-footer">
+          <button class="btn btn-raised btn-secondary" type="button" data-dismiss="modal">Cancelar</button>&nbsp;
+          <a class="btn btn-raised btn-danger" href="logout.php">Cerrar Sesión</a>
+        </div>
+    </div>
+  </div>
+</div>
+<div class="content-wrapper">
+  <div class="content-header admin-panel">      
+    <div class="text-center">
+      <h1>CONFIGURACIÓN</h1>
+    </div>
+    <a href="Config.php?modulo=AddUser"><button class="btn btn-info">Añadir admin</button></a>
+  </div>
+ 
   <div class="wrapper2">
       <div class="card">
         <div class="card-header card-primary">
